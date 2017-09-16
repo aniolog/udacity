b8a8db9a9867e846e68f41e1aa9fbeb645cd61b2
aniolog <aniolog@gmail.com>
2017-09-16 12:17:03 -0400

    fully working Meme me app v2

---
+++ Excercises/ios-nd-uikit-cont.-swift-3/step5.6-bondVillains-masterDetail/BondVillains/VillainCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  VillainCollectionViewCell.swift
+//  BondVillains
+//
+//  Created by Carlos Lozano on 8/21/17.
+//  Copyright © 2017 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class VillainCollectionViewCell: UICollectionViewCell {
+    
+}
